37d9fd2ec5c2e68b1aca1a6e7daac4eec6dc0f9e
Gaurav Singh <gaurav.singh55@gmail.com>
2017-09-25 22:30:50 +0530

    Decisions

---
+++ projects/decisions_ex/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace decisions_ex
+{
+    public class Decisions {
+        public static void Decide(string input) {
+            if (string.IsNullOrEmpty(input)) {
+                Console.WriteLine("Provide valid input");
+            }
+            else {
+                Console.WriteLine($"Input is valid {input}");
+            }
+        }
+    }
+
+    public class ElaborateDecisions {
+        public static void SwitchDemo(string input) {
+            switch (input)
+            {
+                case "Mon":
+                    Console.WriteLine("Its Monday");
+                    break;
+                default:
+                    Console.WriteLine("Ackk Error!!");
+                    break;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Decisions.Decide("");
+            Decisions.Decide("Yahoo!!");
+
+            ElaborateDecisions.SwitchDemo("Mon");
+            ElaborateDecisions.SwitchDemo("");
+        }
+    }
+}
--- projects/strings_ex/Program.cs
+++ projects/strings_ex/Program.cs
@@ -15,7 +15,33 @@ namespace strings_ex
             string nullString = null;
 
             Console.WriteLine($"Length of empty string {emptyString.Length}");
-            Console.WriteLine($"Length of null string {nullString.Length}");
+            // Below will through NullReferenceException
+            // Console.WriteLine($"Length of null string {nullString.Length}"); 
+
+            string fname = "Gaurav";
+            string lname = "Singh";
+
+            Console.WriteLine(fname + " " + lname);
+
+            // Additional string functions:
+            string test = "Test_String";
+            Console.WriteLine(test.ToUpper());
+            Console.WriteLine(test.ToLower());
+
+            string addn = " Steve ";
+            Console.WriteLine(addn.TrimStart());
+            Console.WriteLine(addn.TrimEnd());
+            Console.WriteLine(addn.Trim());
+            Console.WriteLine(addn.Substring(0, 2));
+
+            // Replacing part of strings
+            lastName = "Rockbottom";
+
+            Console.WriteLine($"Hello mr {lastName}"); // string interpolation - recommended
+            Console.WriteLine(String.Format("Hello mr {0}", lastName));
+            Console.WriteLine("Hello mr " + lastName);
+            Console.WriteLine("Hello mr **NAME**".Replace("**NAME**", lastName)); // template way
+
         }
     }
 }
