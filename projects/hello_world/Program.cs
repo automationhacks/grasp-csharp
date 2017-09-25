8d529fcf776e612a9191c11b9cef14b720112643
Gaurav Singh <gaurav.singh55@gmail.com>
2017-09-25 21:23:16 +0530

    Adding latest files

---
+++ projects/hello_world/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; // Library which stores the Console class
+
+namespace projects // a.k.a Folders, used to organize program constructs
+{
+    class Program // Program class
+    {
+        static void Main(string[] args)
+        {
+            var name = "Gaurav";
+            Console.WriteLine("Hello " + name); // Console is static with a WriteLine method
+            Console.WriteLine($"Hello {name}!"); // Another variation of string substitution
+        }
+    }
+}
---
+++ projects/strings_ex/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace strings_ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            name = "Steven";
+            string lastName = "Smith";
+            Console.WriteLine($"{name} {lastName}");
+
+            string emptyString = string.Empty;
+            string nullString = null;
+
+            Console.WriteLine($"Length of empty string {emptyString.Length}");
+            Console.WriteLine($"Length of null string {nullString.Length}");
+        }
+    }
+}
