7a307cb451be9568b7bfe0442b762098a8e71e0d
Gaurav Singh <gaurav.singh55@gmail.com>
2017-10-29 22:26:43 +0530

    Loop examples

---
+++ projects/basics/loop_over_collections/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace loop_over_collections
+{
+    class Program
+    {
+
+        static void forEachDemo() {
+            var myList = new List<string>() {
+                "red",
+                "blue",
+                "green"
+            };
+
+            // Method one to iterate over collection
+            foreach(var item in myList) {
+                Console.WriteLine(item);
+            }
+
+            // ForEach method can be used in case a single operation needs to
+            // be applied for each item of the list
+            Console.WriteLine("=========");
+            myList.ForEach(Console.WriteLine);
+        }
+
+        static void Main(string[] args)
+        {
+            forEachDemo();
+        }
+    }
+}
---
+++ projects/practical_programs/loop_ex_1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Write a program that initializes a list integer numbers, 
+and then prints the numbers out along with their sum. Sample output:
+
+Numbers: 2 4 6
+Sum: 12
+
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace loop_ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intList = new List<int>() {2, 4, 6, 8};
+            var sum = 0;
+            for (int i=0; i < intList.Count; i++) {
+                sum += intList[i];
+                Console.Write($"{intList[i]} ");
+            }
+            Console.WriteLine($"Sum = {sum}");
+        }
+    }
+}
