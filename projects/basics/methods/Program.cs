0a57a6079fb6c262040c39bbaae2339d1361e9a5
Gaurav Singh <gaurav.singh55@gmail.com>
2017-10-30 22:16:54 +0530

    Methods examples

---
+++ projects/basics/methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace methods
+{
+    class Program
+    {
+        public static string firstName;
+        public static string lastName;
+
+        static int staticMethod(int num1, int num2) {
+            return num1 + num2;
+        }
+
+        string instanceMethod() {
+            return $"{firstName} {lastName}";
+        }
+
+        static void Main(string[] args)
+        {
+            firstName = "Gaurav";
+            lastName = "Singh";
+            Program program = new Program();
+            Console.WriteLine(staticMethod(1, 20));
+            Console.WriteLine(program.instanceMethod());
+        }
+    }
+}
