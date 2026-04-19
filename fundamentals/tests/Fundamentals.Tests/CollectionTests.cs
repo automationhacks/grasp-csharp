63e77cee8f9488151b15b494f2d2c0e4e829f815
automationhacks <automationhacks@gmail.com>
2026-04-19 17:04:05 +0530

    Added example of range and spread operator

--- fundamentals/tests/Fundamentals.Tests/CollectionTests.cs
+++ fundamentals/tests/Fundamentals.Tests/CollectionTests.cs
@@ -13,4 +13,16 @@ public class CollectionTests
         List<string> names = ["Alice", "Bob", "Sam"];
         Assert.Equal(3, names.Count);
     }
+
+    [Fact]
+    public void SpreadOperatorTest()
+    {
+        int[] numbers = [1, 2, 3, 4, 5];
+        int[] anotherNumbers = [.. numbers, 6, 7, 8, 9, 10];
+        Assert.Equal(1, anotherNumbers[0]);
+        // Here ^1 refers to the last number in the collection
+        Assert.Equal(10, anotherNumbers[^1]);
+        // Use .. to define a range
+        Assert.Equal([2, 3, 4], anotherNumbers[1..4]);
+    }
 }
